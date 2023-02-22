b96ea8e9f20bc55d864ffb5e7b308075c0ed84ca
SukthiSulaya <125949308+SukthiSulaya@users.noreply.github.com>
2023-02-22 14:08:14 +0530

    Add files via upload

---
+++ asgn1_5/Assignment1_5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Assignment1_5
+{
+    class AsgnCircle
+    {
+        public double circumference(double r)
+        {
+            double a1 = 2 * 3.142 * r;
+            return a1;
+        }
+        public double area(double r)
+        {
+            double a2 = 3.142 * r * r;
+            return a2;
+        }
+        public static void Main()
+        {
+            AsgnCircle c = new AsgnCircle();
+            double rad, ar, cir;
+            Console.WriteLine("Enter the radius : ");
+            rad = Convert.ToDouble(Console.ReadLine());
+            ar = c.area(rad);
+            cir = c.circumference(rad);
+            Console.WriteLine("area of the circle is {0}",  ar);
+            Console.WriteLine("Circumference of circle is {0} ",  cir);
+            Console.ReadLine();
+        }
+    }
+}
+
